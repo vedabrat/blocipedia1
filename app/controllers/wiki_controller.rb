eac1cdaee2ae57ac2110f00b202906dbcc8f2561
vedabrat <vedabrat>
2017-12-21 19:17:45 -0500

    wiki model added

---
+++ app/controllers/wiki_controller.rb
@@ -0,0 +1,42 @@
+class WikiController < ApplicationController
+  def index
+    @wiki = Wiki.all
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+     if @wiki.save
+       flash[:notice] = "wiki was saved."
+       redirect_to @wiki
+     else
+       flash.now[:alert] = "There was an error saving the wiki. Please try again."
+       render :new
+     end
+  end
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def destroy
+     @wiki = Wiki.find(params[:id])
+ 
+     if @wiki.destroy
+       flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+       redirect_to wikis_path
+     else
+       flash.now[:alert] = "There was an error deleting the wiki."
+       render :show
+     end
+  end
+end
---
+++ spec/controllers/wiki_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe WikiController, type: :controller do
+
+
+let(:wiki) {Wiki.create!(title: "random string" , body: "random text" , private: boolean , user: "random name")}
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns http success" do
+      get :edit
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
