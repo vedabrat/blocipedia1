c7dd275df31c7884345e5aa89dd97a666e775a96
JessePledger <JessePledger@gmail.com>
2018-01-18 12:50:04 -0500

    Fix collaborator pg indexes

--- db/migrate/20180115070207_create_collaborators.rb
+++ db/migrate/20180115070207_create_collaborators.rb
@@ -5,9 +5,5 @@ class CreateCollaborators < ActiveRecord::Migration[5.1]
        t.belongs_to :user, index: true
        t.timestamps null: false
     end
-    add_index :collaborators, :id, unique: true
-    add_index :collaborators, :user_id
-    add_index :collaborators, :wiki_id
-
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180104202848) do
+ActiveRecord::Schema.define(version: 20180115070207) do
+
+  create_table "collaborators", force: :cascade do |t|
+    t.integer "wiki_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_collaborators_on_user_id"
+    t.index ["wiki_id"], name: "index_collaborators_on_wiki_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
