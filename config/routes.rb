08044bcb30128748a638f513846b333738280832
vedabrat <vedabrat>
2018-01-18 18:52:10 -0500

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :wikis
   get 'wikis/new'
   resources :charges, only: [:new, :create]
-  root to: 'welcome#index'
+  root to: 'wikis#index'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
